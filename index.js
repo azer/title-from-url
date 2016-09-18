145f9b18e9d3cb075bae6f70d3505196ca01fb66
E.Azer Koçulu <azer@kodfabrik.com>
2016-09-18 09:09:03 -0700

    remove qs

--- index.js
+++ index.js
@@ -9,25 +9,17 @@ function urlToTitle (url) {
   url = url.replace(/(\/|\?)$/, '');
 
   var parts = url.split('?');
-  var qs = parts[1];
   url = parts[0];
   url = url.replace(/\.\w+$/, '');
 
   parts = url.split('/');
 
   var name = parts[0];
-  name = name.replace(/\.\w+(\/|$)/, '');
+  name = name.replace(/\.\w+(\/|$)/, '').replace(/\.(com?|net|org)$/, '')
 
   if (parts.length == 1) {
     return toTitle(name);
   }
 
-  if (qs) {
-    qs = qs.replace(/\=/g, ':').replace(/\&/g, ', ');
-    qs = ' (' + qs.slice(0, 25) + (qs.length > 25 ? '...' : '') + ')';
-  } else {
-    qs = '';
-  }
-
-  return toTitle(parts.slice(1).reverse().join(' - ')) + qs + ' on ' + toTitle(name.split('.').join(', '));
+  return toTitle(parts.slice(1).reverse().join(' - ')) + ' on ' + toTitle(name.split('.').join(', '));
 }
--- test.js
+++ test.js
@@ -2,11 +2,13 @@ var test = require("prova");
 var title = require("./");
 
 test('generate a title from url', function (t) {
-  t.plan(7);
+  t.plan(9);
   t.equal(title('http://wikipedia.org'), 'Wikipedia');
   t.equal(title('http://wikipedia.org/'), 'Wikipedia');
   t.equal(title('https://en.wikipedia.org/wiki/Gezi'), 'Gezi - Wiki on En, Wikipedia');
-  t.equal(title('encrypted.google.com/search?q=foo&bar=qux'), 'Search (q:foo, bar:qux) on Encrypted, Google');
+  t.equal(title('encrypted.google.com.tr/search?q=foo&bar=qux'), 'Search on Encrypted, Google');
+  t.equal(title('encrypted.google.co.uk/search?q=foo&bar=qux'), 'Search on Encrypted, Google');
+  t.equal(title('encrypted.google.org.tr/search?q=foo&bar=qux'), 'Search on Encrypted, Google');
   t.equal(title('azer.bike/f.o-o/bar.html'), 'Bar - F.o-o on Azer');
   t.equal(title('http://ada.io'), 'Ada');
   t.equal(title('http://en.wikipedia.org/wiki/Foo_%28app%29'), 'Foo (app) - Wiki on En, Wikipedia');
