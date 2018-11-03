b742f1420865479a09aa9a474cc5a6585ea76e3a
E.Azer Koçulu <azer@kodfabrik.com>
2018-11-04 07:28:47 +0800

    Filter out numbers & words shorter than 2 letters

--- index.js
+++ index.js
@@ -1,33 +1,53 @@
-var toTitle = require("to-title");
+var toTitle = require("to-title")
 
-module.exports = urlToTitle;
+module.exports = urlToTitle
 
-function urlToTitle (url) {
-  url = unescape(url).replace(/_/g, ' ');
-  url = url.replace(/^\w+:\/\//, '');
-  url = url.replace(/^www\./, '');
-  url = url.replace(/(\/|\?)$/, '');
+function urlToTitle(url) {
+  url = unescape(url).replace(/_/g, " ")
+  url = url.replace(/^\w+:\/\//, "")
+  url = url.replace(/^www\./, "")
+  url = url.replace(/(\/|\?)$/, "")
 
-  var parts = url.split('?');
-  url = parts[0];
-  url = url.replace(/\.\w+$/, '');
+  var parts = url.split("?")
+  url = parts[0]
+  url = url.replace(/\.\w+$/, "")
 
-  parts = url.split('/');
+  parts = url.split("/")
 
-  var name = parts[0];
-  name = name.replace(/\.\w+(\/|$)/, '').replace(/\.(com?|net|org|fr)$/, '')
+  var name = parts[0]
+  name = name.replace(/\.\w+(\/|$)/, "").replace(/\.(com?|net|org|fr)$/, "")
 
   if (parts.length == 1) {
-    return title(name);
+    return title(name)
   }
 
-  return toTitle(parts.slice(1).reverse().map(toTitle).join(' - ')) + ' on ' + title(name);
+  return (
+    toTitle(
+      parts
+        .slice(1)
+        .reverse()
+        .filter(isValidPart)
+        .map(toTitle)
+        .join(" - ")
+    ) +
+    " on " +
+    title(name)
+  )
 }
 
-function title (host) {
+function isValidPart(part) {
+  return part.length > 2 && !/^[0-9]+$/.test(part)
+}
+
+function title(host) {
   if (/^[\w\.\-]+:\d+/.test(host)) {
     return host
   }
 
-  return toTitle(host.split('.').join(', '))
+  return toTitle(
+    host
+      .split(".")
+      .filter(isValidPart)
+      .join(", ")
+  )
 }
--- test.js
+++ test.js
@@ -1,18 +1,40 @@
-var test = require("prova");
-var title = require("./");
+var test = require("prova")
+var title = require("./")
 
-test('generate a title from url', function (t) {
-  t.plan(12);
-  t.equal(title('http://wikipedia.org'), 'Wikipedia');
-  t.equal(title('http://wikipedia.org/'), 'Wikipedia');
-  t.equal(title('https://en.wikipedia.org/wiki/Gezi'), 'Gezi - Wiki on En, Wikipedia');
-  t.equal(title('encrypted.google.com.tr/search?q=foo&bar=qux'), 'Search on Encrypted, Google');
-  t.equal(title('encrypted.google.co.uk/search?q=foo&bar=qux'), 'Search on Encrypted, Google');
-  t.equal(title('encrypted.google.org.tr/search?q=foo&bar=qux'), 'Search on Encrypted, Google');
-  t.equal(title('azer.bike/f.o-o/bar.html'), 'Bar - F O O on Azer');
-  t.equal(title('http://ada.io'), 'Ada');
-  t.equal(title('http://en.wikipedia.org/wiki/Foo_%28app%29'), 'Foo (app) - Wiki on En, Wikipedia');
-  t.equal(title('http://hacks.mozilla.org/2014/06/breaking-the-borders-of-indexeddb'), 'Breaking the Borders of Indexeddb - 06 - 2014 on Hacks, Mozilla')
-  t.equal(title('localhost:8080'), 'localhost:8080')
-  t.equal(title('http://yo.lo:8080/foo/bar'), 'Bar - Foo on yo.lo:8080')
-});
+test("generate a title from url", function(t) {
+  t.plan(13)
+  t.equal(title("http://wikipedia.org"), "Wikipedia")
+  t.equal(title("http://wikipedia.org/"), "Wikipedia")
+  t.equal(
+    title("https://en.wikipedia.org/wiki/Gezi"),
+    "Gezi - Wiki on Wikipedia"
+  )
+  t.equal(
+    title("encrypted.google.com.tr/search?q=foo&bar=qux"),
+    "Search on Encrypted, Google"
+  )
+  t.equal(
+    title("encrypted.google.co.uk/search?q=foo&bar=qux"),
+    "Search on Encrypted, Google"
+  )
+  t.equal(
+    title("encrypted.google.org.tr/search?q=foo&bar=qux"),
+    "Search on Encrypted, Google"
+  )
+  t.equal(title("azer.bike/f.o-o/bar.html"), "Bar - F O O on Azer")
+  t.equal(title("http://ada.io"), "Ada")
+  t.equal(
+    title("http://en.wikipedia.org/wiki/Foo_%28app%29"),
+    "Foo (app) - Wiki on Wikipedia"
+  )
+  t.equal(
+    title("http://hacks.mozilla.org/2014/06/breaking-the-borders-of-indexeddb"),
+    "Breaking the Borders of Indexeddb on Hacks, Mozilla"
+  )
+  t.equal(
+    title("https://twitter.com/naval/status/1002103360646823936?lang=en"),
+    "Status - Naval on Twitter"
+  )
+  t.equal(title("localhost:8080"), "localhost:8080")
+  t.equal(title("http://yo.lo:8080/foo/bar"), "Bar - Foo on yo.lo:8080")
+})
